82a7621209b6a4e6c331fde9f0140a9a058d82af
Trollkopf <max.serratosa@gmail.com>
2025-09-24 13:21:05 +0200

    added backoffice

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,12 +1,12 @@
 <?php
-define('BASE_PATH', dirname(__DIR__));   // raíz del proyecto
+define('BASE_PATH', dirname(__DIR__).'/..');   // raíz del proyecto
 
 session_start();
-require BASE_PATH . '/../src/Support/Config.php';
-require BASE_PATH . '/../src/Core/DB.php';
-require BASE_PATH . '/../app/config/auth.php';
-require BASE_PATH . '/../app/Controllers/AuthController.php';
-require BASE_PATH . '/../app/Controllers/MemberController.php';
+require BASE_PATH . '/src/Support/Config.php';
+require BASE_PATH . '/src/Core/DB.php';
+require BASE_PATH . '/app/config/auth.php';
+require BASE_PATH . '/app/Controllers/AuthController.php';
+require BASE_PATH . '/app/Controllers/MemberController.php';
 
 $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $uri = preg_replace('#^admin/?#', '', $uri); // quitar prefijo admin
