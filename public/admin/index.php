8e1c4664122d10788339c541f1d1181ccdfde8e0
Trollkopf <max.serratosa@gmail.com>
2025-09-24 12:46:22 +0200

    added backoffice

--- public/admin/index.php
+++ public/admin/index.php
@@ -7,27 +7,27 @@ $APP = dirname(__DIR__, 2);
 error_log('__DIR__ = ' . __DIR__);
 error_log('APP = ' . $APP);
 
-// session_start();
-// $ROOT = dirname(__DIR__, 1);           // .../public
-// $APP  = dirname($ROOT);                // raiz proyecto
-// require $APP . '/src/Support/Config.php';
-// require $APP . '/src/Core/DB.php';
-// require $APP . '/app/config/auth.php';
-// require $APP . '/app/controllers/AuthController.php';
-// require $APP . '/app/controllers/MemberController.php';
+ session_start();
+$ROOT = dirname(__DIR__, 1);           // .../public
+$APP  = dirname($ROOT);                // raiz proyecto
+require $APP . '/src/Support/Config.php';
+require $APP . '/src/Core/DB.php';
+require $APP . '/app/config/auth.php';
+require $APP . '/app/controllers/AuthController.php';
+require $APP . '/app/controllers/MemberController.php';
 
-// $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-// $uri = preg_replace('#^admin/?#', '', $uri); // quitar prefijo admin
+$uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$uri = preg_replace('#^admin/?#', '', $uri); // quitar prefijo admin
 
-// // rutas
-// if ($uri === '' || $uri === 'members')                return \App\Controllers\MemberController::index();
-// if ($uri === 'members/create')                        return \App\Controllers\MemberController::create();
-// if ($uri === 'members/store' && $_SERVER['REQUEST_METHOD']==='POST') return \App\Controllers\MemberController::store();
-// if (preg_match('#^members/(\d+)/edit$#', $uri, $m))   return \App\Controllers\MemberController::edit((int)$m[1]);
-// if (preg_match('#^members/(\d+)/update$#', $uri, $m) && $_SERVER['REQUEST_METHOD']==='POST') return \App\Controllers\MemberController::update((int)$m[1]);
-// if (preg_match('#^members/(\d+)/toggle$#', $uri, $m)) return \App\Controllers\MemberController::toggle((int)$m[1]);
-// if (preg_match('#^members/(\d+)/delete$#', $uri, $m)) return \App\Controllers\MemberController::delete((int)$m[1]);
+// rutas
+if ($uri === '' || $uri === 'members')                return \App\Controllers\MemberController::index();
+if ($uri === 'members/create')                        return \App\Controllers\MemberController::create();
+if ($uri === 'members/store' && $_SERVER['REQUEST_METHOD']==='POST') return \App\Controllers\MemberController::store();
+if (preg_match('#^members/(\d+)/edit$#', $uri, $m))   return \App\Controllers\MemberController::edit((int)$m[1]);
+if (preg_match('#^members/(\d+)/update$#', $uri, $m) && $_SERVER['REQUEST_METHOD']==='POST') return \App\Controllers\MemberController::update((int)$m[1]);
+if (preg_match('#^members/(\d+)/toggle$#', $uri, $m)) return \App\Controllers\MemberController::toggle((int)$m[1]);
+if (preg_match('#^members/(\d+)/delete$#', $uri, $m)) return \App\Controllers\MemberController::delete((int)$m[1]);
 
-// if ($uri === 'login')                                  return \App\Controllers\AuthController::login();
-// if ($uri === 'logout')                                 return \App\Controllers\AuthController::logout();
-// http_response_code(404); echo '404';
+if ($uri === 'login')                                  return \App\Controllers\AuthController::login();
+if ($uri === 'logout')                                 return \App\Controllers\AuthController::logout();
+http_response_code(404); echo '404';
